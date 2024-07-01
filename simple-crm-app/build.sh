b76c0f8787f84534f721b195367c291e8d0b8807
carlvin <calvinokumu@gmail.com>
2024-07-01 16:12:00 +0300

    vercel files

--- simple-crm-app/build.sh
+++ simple-crm-app/build.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.freeze
+python3.9 pip install -r requirements.freeze
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput --clear
