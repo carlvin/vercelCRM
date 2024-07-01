57285001288e60c4b7fd5cd943bdcc649d8aa6fa
Craimo <s223263086@deakin.edu.au>
2024-07-01 11:38:09 +0300

    Initial dev commit

---
+++ simple-crm-app/config/settings/dev_neon.py
@@ -0,0 +1,28 @@
+from .base import *
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ['https://*.carlhub.com','https://*.127.0.0.1']
+
+INTERNAL_IPS = ['127.0.0.1',]
+
+ALLOWED_HOSTS = ['carlhub.com','www.carlhub.com','localhost','127.0.0.1']
+
+SECRET_KEY="django-insecure-h*wprd2xbt#!jq#rrkr_ww2h(md#=bm&a0ok4h!tp(081%+6)6"
+
+MEDIA_ROOT = "/home/media"
+STATIC_ROOT = "/home/static"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['PGDATABASE'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        # 'HOST': os.getenv('DB_HOST'),
+        'HOST':os.environ['PGHOST'],
+        'PORT':os.environ.get('PGPORT','5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
+}
